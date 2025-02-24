69a359e576efd530f208a55a205ff600d0da21f5
Max Shipit <max@shipit.dev>
2025-02-24 17:10:16 +0100

    release version 0.0.3

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
-// Reexport your entry components here
+import { auth, init, createRecordStore, createRecordsStore } from "./stores.svelte.js";
+import Record from "./components/Record.svelte";
+import Records from "./components/Records.svelte";
+
+export { auth, init, createRecordStore, createRecordsStore, Record, Records };
